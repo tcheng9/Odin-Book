a43a3cc830c516afb4ec6bb416442b41fb4722af
Tommy C <tcheng789@gmail.com>
2022-10-29 14:52:11 -0500

    mongoose setup

--- Odin-Book/app.js
+++ Odin-Book/app.js
@@ -6,8 +6,25 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var profileRouter = require('./routes/profile');
+var showPageRouter = require('./routes/showPage');
+var signupRouter = require('./routes/signup');
+var timelineRouter = require('./routes/timeline');
+require("dotenv").config();
 
 var app = express();
+///Mongoose setup
+
+//Url
+
+const mongoPw = process.env.MONGOPW;
+const mongoDb = "mongodb+srv://tcheng:" + mongoPw + "@cluster0.noy7cwp.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +36,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
+//
+/////////NOTE: HOME PAGE HAS TO BE LOGIN PAGE
+//
 app.use('/', indexRouter);
+// app.use('/', loginRouter);
 app.use('/users', usersRouter);
+app.use('/profile', profileRouter);
+app.use('/showpage', showPageRouter);
+app.use('/signup', signupRouter);
+app.use('/timeline', timelineRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,3 +68,6 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+const PORT = 4000;
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
